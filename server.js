f08449d4b27312b3c2bae5c0f60ebcdfa39c1e37
AR-92 <electro.electra@gmail.com>
2025-11-20 17:59:16 +0300

    Remove all files to initialize mini-accelerator branch

--- server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const bootstrap = require('./src/bootstrap');
-
-// Start the application
-bootstrap().catch((error) => {
-  console.error('Failed to start application:', error);
-  process.exit(1);
-});
